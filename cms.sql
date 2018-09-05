8864df05c611e77a0573d425de60c3a966f3d0ae
ChoelWu <38238531+ChoelWu@users.noreply.github.com>
2018-09-05 18:11:29 +0800

    back-up sql
    back-up sql

--- cms.sql
+++ cms.sql
@@ -10,10 +10,44 @@ Target Server Type    : MYSQL
 Target Server Version : 50721
 File Encoding         : 65001
 
-Date: 2018-08-30 19:08:25
+Date: 2018-09-05 18:10:55
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for cms_article
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_article`;
+CREATE TABLE `cms_article` (
+  `id` varchar(32) NOT NULL,
+  `title` varchar(100) NOT NULL COMMENT '标题',
+  `is_title_bold` char(1) DEFAULT '0' COMMENT '标题加粗 0-不加粗 1-加粗',
+  `is_title_italic` char(1) DEFAULT '0' COMMENT '标题加粗 0-不倾斜 1-倾斜',
+  `title_color` char(7) DEFAULT '' COMMENT '标题颜色',
+  `user_id` varchar(32) DEFAULT NULL COMMENT '作者',
+  `nav_id` varchar(32) DEFAULT NULL COMMENT '所属导航',
+  `tag_id` varchar(32) DEFAULT '0' COMMENT '所属标签',
+  `view_number` int(11) NOT NULL DEFAULT '0' COMMENT '浏览次数',
+  `thumb_img` varchar(100) DEFAULT NULL COMMENT '缩略图',
+  `is_top` char(1) NOT NULL DEFAULT '0' COMMENT '是否置顶 1-是 0-否',
+  `summary` varchar(600) DEFAULT '0' COMMENT '文章简介',
+  `source` varchar(50) DEFAULT '' COMMENT '来源',
+  `content` text COMMENT '内容',
+  `publish_date` date DEFAULT NULL COMMENT '发布日期',
+  `status` char(1) NOT NULL DEFAULT '1' COMMENT '状态 1-未发布（默认） 2-已发布 0-删除',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_article_title` (`title`),
+  KEY `idx_article_user_id` (`user_id`) USING BTREE,
+  KEY `idx_article_tag_id` (`tag_id`) USING BTREE,
+  KEY `idx_article_nav_id` (`nav_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of cms_article
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for cms_authorize
@@ -60,8 +94,40 @@ INSERT INTO `cms_menu` VALUES ('MENU_201808300232288030', '系统管理', '1', '
 INSERT INTO `cms_menu` VALUES ('MENU_201808300237076705', '菜单管理', '2', 'MENU_201808300232288030', 'book', '0101', 'admin/menu/index', '1', '2018-08-30 02:37:07', '2018-08-30 02:37:07');
 INSERT INTO `cms_menu` VALUES ('MENU_201808300240082440', '权限管理', '2', 'MENU_201808300232288030', 'user', '0102', 'admin/rule/index', '1', '2018-08-30 02:40:08', '2018-08-30 02:40:08');
 INSERT INTO `cms_menu` VALUES ('MENU_201808300241344594', '角色管理', '2', 'MENU_201808300232288030', 'user', '0103', 'admin/role/index', '1', '2018-08-30 02:41:34', '2018-08-30 02:41:34');
-INSERT INTO `cms_menu` VALUES ('MENU_201808300242103500', '用户管理', '2', 'MENU_201808300232288030', 'user', '1014', 'admin/user/index', '1', '2018-08-30 02:42:10', '2018-08-30 02:42:10');
-INSERT INTO `cms_menu` VALUES ('MENU_201808300321167235', '测试菜单', '1', '0', 'book', '0200', '#', '1', '2018-08-30 03:21:16', '2018-08-30 03:30:52');
+INSERT INTO `cms_menu` VALUES ('MENU_201808300242103500', '用户管理', '2', 'MENU_201808300232288030', 'user', '0104', 'admin/user/index', '1', '2018-08-30 02:42:10', '2018-09-01 02:25:28');
+INSERT INTO `cms_menu` VALUES ('MENU_201809010226592963', '基本信息设置', '2', 'MENU_201808300232288030', 'gear', '0105', 'admin/info/index', '1', '2018-09-01 02:26:59', '2018-09-01 02:33:57');
+INSERT INTO `cms_menu` VALUES ('MENU_201809010246201199', '内容管理', '1', '0', 'files-o', '0200', '#', '1', '2018-09-01 02:46:20', '2018-09-01 02:46:20');
+INSERT INTO `cms_menu` VALUES ('MENU_201809010252314085', '导航管理', '2', 'MENU_201809010246201199', 'sitemap', '0201', 'admin/nav/index', '1', '2018-09-01 02:52:31', '2018-09-01 02:52:31');
+INSERT INTO `cms_menu` VALUES ('MENU_201809010315324304', '文章管理', '2', 'MENU_201809010246201199', 'file', '0203', 'admin/article/index', '1', '2018-09-01 03:15:32', '2018-09-05 09:43:25');
+INSERT INTO `cms_menu` VALUES ('MENU_201809050943051002', '标签管理', '2', 'MENU_201809010246201199', 'tag', '0202', 'admin/tag/index', '1', '2018-09-05 09:43:05', '2018-09-05 09:43:35');
+
+-- ----------------------------
+-- Table structure for cms_nav
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_nav`;
+CREATE TABLE `cms_nav` (
+  `id` varchar(32) NOT NULL COMMENT '导航id',
+  `name` varchar(30) NOT NULL COMMENT '导航名称',
+  `level` char(1) NOT NULL DEFAULT '1' COMMENT '导航级别',
+  `parent_id` char(32) NOT NULL DEFAULT '0' COMMENT '导航父栏目编号',
+  `icon` varchar(20) DEFAULT NULL COMMENT '导航图标',
+  `sort` varchar(10) NOT NULL DEFAULT '0' COMMENT '排序',
+  `thumb_img` varchar(100) DEFAULT NULL COMMENT '缩略图',
+  `summary` varchar(600) DEFAULT NULL COMMENT '导航简介',
+  `url` varchar(50) DEFAULT NULL COMMENT '导航地址',
+  `status` char(1) NOT NULL DEFAULT '1' COMMENT '导航状态，0-停用1-启用',
+  `created_at` datetime DEFAULT NULL COMMENT '创建时间',
+  `updated_at` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_nav_parent_id` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of cms_nav
+-- ----------------------------
+INSERT INTO `cms_nav` VALUES ('NAV_201809030640057728', '导航一', '1', '0', 'book', '0100', null, null, 'www.baidu.com', '1', '2018-09-03 06:40:05', '2018-09-03 06:40:05');
+INSERT INTO `cms_nav` VALUES ('NAV_201809050936235569', '导航二', '1', '0', 'book', '0200', null, null, 'https://www.baidu.com', '1', '2018-09-05 09:36:23', '2018-09-05 09:36:33');
+INSERT INTO `cms_nav` VALUES ('NAV_201809050939183546', '导航三', '1', '0', 'gear', '0300', null, null, 'www.baidu.com', '1', '2018-09-05 09:39:18', '2018-09-05 09:39:18');
 
 -- ----------------------------
 -- Table structure for cms_role
@@ -80,7 +146,8 @@ CREATE TABLE `cms_role` (
 -- Records of cms_role
 -- ----------------------------
 INSERT INTO `cms_role` VALUES ('ROLE_201808300246497861', '角色一', '1', '2018-08-30 02:46:49', '2018-08-30 02:46:49');
-INSERT INTO `cms_role` VALUES ('1', '超级管理员', '1', '2018-08-15 06:29:28', '2018-08-15 06:46:00');
+INSERT INTO `cms_role` VALUES ('1', '管理员', '1', '2018-08-15 06:29:28', '2018-08-15 06:46:00');
+INSERT INTO `cms_role` VALUES ('ROLE_201809040927107504', 'ceshi', '1', '2018-09-04 09:27:10', '2018-09-04 09:27:10');
 
 -- ----------------------------
 -- Table structure for cms_rule
@@ -111,6 +178,26 @@ INSERT INTO `cms_rule` VALUES ('RULE_201808300245588259', '获取菜单状态', 
 INSERT INTO `cms_rule` VALUES ('RULE_201808300246294010', '权限列表', 'admin/rule/index', 'MENU_201808300240082440', '0201', '1', '2018-08-30 02:46:29', '2018-08-30 02:46:29');
 
 -- ----------------------------
+-- Table structure for cms_tag
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_tag`;
+CREATE TABLE `cms_tag` (
+  `id` varchar(32) NOT NULL,
+  `name` varchar(60) NOT NULL DEFAULT '' COMMENT '路由规则',
+  `status` char(1) NOT NULL DEFAULT '1' COMMENT '状态 1-启用 0-禁用',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of cms_tag
+-- ----------------------------
+INSERT INTO `cms_tag` VALUES ('1', '标签1', '1', null, '2018-09-05 10:06:26');
+INSERT INTO `cms_tag` VALUES ('TAG_201809051007124367', '标签2', '1', '2018-09-05 10:07:12', '2018-09-05 10:07:12');
+INSERT INTO `cms_tag` VALUES ('TAG_201809051008111335', '标签3', '1', '2018-09-05 10:08:11', '2018-09-05 10:08:11');
+
+-- ----------------------------
 -- Table structure for cms_user
 -- ----------------------------
 DROP TABLE IF EXISTS `cms_user`;
@@ -119,8 +206,8 @@ CREATE TABLE `cms_user` (
   `account` varchar(20) NOT NULL COMMENT '账户名',
   `nickname` varchar(30) NOT NULL COMMENT '昵称',
   `password` char(32) NOT NULL COMMENT '密码',
-  `identify` char(32) DEFAULT NULL COMMENT '第二身份',
-  `token` char(32) DEFAULT NULL COMMENT '令牌',
+  `identify` char(100) DEFAULT NULL COMMENT '第二身份',
+  `token` char(100) DEFAULT NULL COMMENT '令牌',
   `deadline` datetime DEFAULT NULL COMMENT '令牌过期时间',
   `e_mail` varchar(30) DEFAULT NULL COMMENT '电子邮件',
   `phone` varchar(20) DEFAULT NULL COMMENT '电话',
@@ -134,8 +221,8 @@ CREATE TABLE `cms_user` (
 -- ----------------------------
 -- Records of cms_user
 -- ----------------------------
-INSERT INTO `cms_user` VALUES ('1', 'admin', '超级管理员', '35e078bcb1e78a69c9a88acbe79cefc2', null, null, null, '775669127@qq.com', '18894330931', '1', 'user/header_img/201807190229245b4ff78440d3d.png', '2018-07-19 02:29:24', '2018-08-29 02:37:16');
-INSERT INTO `cms_user` VALUES ('USER_201808300301297631', 'choel', 'choel', 'c712d1741bc27b73cb27eb9d0149165e', null, null, null, 'choel_wu@foxmail.com', '18894330931', '1', 'uploads/user/header_img/201808300307175b875f654f594.jpg', '2018-08-30 03:01:29', '2018-08-30 03:10:21');
+INSERT INTO `cms_user` VALUES ('1', 'admin', 'superadmin', '35e078bcb1e78a69c9a88acbe79cefc2', 'NWM5MTg0ZWY0MDFjYTJjNDQ5MzY5NGE4ZjNkNjY5YmY=', 'bc7235a51e34c1d3ebfddeb538c20c71', '2018-09-11 09:14:00', '775669127@qq.com', '18894330931', '1', 'static/admin/img/admin.png', '2018-07-19 02:29:24', '2018-09-04 09:14:00');
+INSERT INTO `cms_user` VALUES ('USER_201808300301297631', 'choel', 'choel', 'c712d1741bc27b73cb27eb9d0149165e', 'MmEyYTEyNjFjNmUzNjI0MGQ1MWYyZDUzMmZlNzE4NzQ=', 'bd4da34a1b327d0d35bad998ef535b02', '2018-09-07 03:53:33', 'choel_wu@foxmail.com', '18894330931', '1', 'uploads/user/header_img/201808300307175b875f654f594.jpg', '2018-08-30 03:01:29', '2018-08-31 03:53:33');
 
 -- ----------------------------
 -- Table structure for cms_user_role
